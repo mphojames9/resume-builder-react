97774acae91a68a424016aaa340d8bcb7d4b84a4
Mpho Matli <mphojames9@gmail.com>
2024-08-06 17:48:11 +0200

    setting up NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavBar.css';
 import { assets } from '../../assets/assets';
 
 const NavBar = () => {
+
+    const [menu,setMenu] = useState("home");
+
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt='logo' />
+      <img src={assets.logo} alt="logo" className="logo" />
+      <ul className="navbar-menu">
+        <ul onClick={()=>setMenu('home')} className={menu === 'home'?'active':''}>home</ul>
+        <ul onClick={()=>setMenu('menu')} className={menu === 'menu'?'active':''}>menu</ul>
+        <ul onClick={()=>setMenu('mobile-app')} className={menu === 'mobile-app'?'active':''}>mobile-app</ul>
+        <ul onClick={()=>setMenu('contact-us')} className={menu === 'contact-us'?'active':''}>contact us</ul>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="search icon" />
+        <div className="navbar-search-icon">
+            <img src={assets.basket_icon} alt="basket icon" />
+            <div className="dot"></div>
+        </div>
+        <button>sign in</button>
+      </div>
     </div>
   );
 }
