f3c6065d8e5734fe35f98ff7c437aec2ee664ebb
Mpho Matli <mphojames9@gmail.com>
2024-08-27 07:04:49 +0200

    update placeOrder components

--- src/pages/PlaceOrder/PlaceOder.jsx
+++ src/pages/PlaceOrder/PlaceOder.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './PlaceOrder.css';
+import { StoreContext } from '../../context/StoreContext';
 
 const PlaceOder = () => {
+
+  const {getTotalCartAmount}  = useContext(StoreContext);
+
   return (
-    <div>
-      
+    <form className='place-order'>
+      <div className='place-order-left'>
+        <p className='title'>Delivery Information</p>
+        <div className='multi-fields'>
+          <input type='text' placeholder='First name' />
+          <input type='text' placeholder='Last name' />
+      </div>
+      <input type='email' placeholder='Email address' />
+      <input type='text' placeholder='Street' />
+      <div className='multi-fields'>
+        <input type='text' placeholder='City' />
+        <input type='text' placeholder='Provance' />
+      </div>
+      <div className='multi-fields'>
+        <input type='text' placeholder='Zip code' />
+        <input type='text' placeholder='Country' />
     </div>
+    <input type='text' placeholder='phone' />
+    </div>
+    <div className='place-order-right'>
+    <div className="cart-total">
+          <h2>Cart Totals</h2>
+        <div>
+    
+        <div className="cart-total-details">
+          <p>Subtotal</p>
+          <p>R{getTotalCartAmount()}</p>
+        </div>
+        <hr />
+        <div className="cart-total-details">
+          <p>Delivery Fee</p>
+          <p>R{150}</p>
+        </div>
+        <hr />
+        <div className="cart-total-details">
+          <b>Total</b>
+          <b>R{getTotalCartAmount()+150}</b>
+        </div>
+      </div>
+      <button>PROCEED TO PAYMENT</button>
+      </div>
+      </div>
+      </form>
+
   );
 }
 
