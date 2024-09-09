da7b9401bffe4f50b6d1151a160844bce1166a6e
Mpho Matli <mphojames9@gmail.com>
2024-09-09 18:50:44 +0200

    setting admin panel, frontend

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
 import './Add.css';
+import { assets } from '../../assets/assets';
 
 const Add = () => {
   return (
-    <div>
+    <div className='add'>
+        <form className="flex-col">
+            <div className="add-img-upload flex-col">
+                <p>Upload Image</p>
+                <label htmlFor='image'>
+                    <img src={assets.upload_area} alt="" />
+                </label>
+                <input type='file' id='image' hidden required />
+            </div>
+        <div className="add-product-name flex-col">
+            <p>Product name</p>
+            <input type='text' name='name' placeholder='Type here' />
+            </div>
+            <div className="add-product-description flex-col">
+                <p>Product description</p>
+            <textarea name='description' rows='6' placeholder='Write content here' required></textarea>
+        </div>
+            <div className="add-category-price">
+                <div className="add-category flex-col">
+                    <p>Product category</p>
+                    <select name="category">
+                        <option value="Accessories">Accessories</option>
+                        <option value="Phones">Phones</option>
+                        <option value="Laptops">Laptops</option>
+                        <option value="Smart TV">Smart TV</option>
+                        <option value="Monitor">Monitor</option>
+                        <option value="Taplets">Taplets</option>
+                        <option value="Office">Office</option>
+                        <option value="Gaming">Gaming</option>
+                    </select>
+                </div>
+                 <div className="add-price flex-col">
+                    <p>Product price</p>
+                    <input type='Number' name='price' placeholder='R200' />
+                 </div>
+            </div>
+            <button className="add-btn" type='submit'>ADD</button>
+        </form>
       
     </div>
   );
